03cfa643ec88616a58794bacb7ce74e4a547623c
mdlufy <zidakbew@gmail.com>
2025-04-09 22:58:39 +0300

    feat: add wrapper

---
+++ README.md
@@ -0,0 +1,9 @@
+## WebView wrapper for testing app on iOS
+
+1. Open project in Xcode
+2. Insert your URL in [ViewController](./WebviewWrapperApp/ViewController.swift#L21)
+3. Build project and see simulation (opened automatically after successfully build)
+4. Open Safari Web Inspector for debug
+5. Testing
+
+![Test webview](assets/webview.png)
---
+++ WebviewWrapperApp/ViewController.swift
@@ -0,0 +1,39 @@
+import UIKit
+import WebKit
+
+class ViewController: UIViewController, WKNavigationDelegate {
+    var webView: WKWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let config = WKWebViewConfiguration()
+        // enable debug
+        config.preferences.setValue(true, forKey: "developerExtrasEnabled")
+        
+        webView = WKWebView(frame: view.bounds, configuration: config)
+        webView.navigationDelegate = self
+        view.addSubview(webView)
+        
+        if #available(iOS 16.4, *) {
+            webView.isInspectable = true
+        }
+        
+        let url = URL(string: "https://taiga-ui.dev/")!
+        webView.load(URLRequest(url: url))
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print("Page successfully loaded")
+    }
+    
+    // For working with untrusted SSL custom certificate
+    func webView(_ webView: WKWebView, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
+        if challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodServerTrust {
+            let credential = URLCredential(trust: challenge.protectionSpace.serverTrust!)
+            completionHandler(.useCredential, credential)
+        } else {
+            completionHandler(.performDefaultHandling, nil)
+        }
+    }
+}
